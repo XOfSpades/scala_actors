b18811a8c9d55fefbea602f716132a844a6ef846
Bernhard Stoecker <stoecki@der-ball-ist-rund.net>
2016-10-11 10:28:33 +0200

    Add supervisors to kids actors

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,6 +1,11 @@
 package runtime
 
 import akka.actor._
+import akka.actor.Props
+import akka.pattern._
+import akka.pattern.BackoffOptionsImpl
+import akka.pattern.Backoff
+import scala.concurrent.duration.DurationInt
 import kid.Kid
 import kid.Poke
 import kid.Feed
@@ -9,8 +14,35 @@ object Main extends App {
 
 	val system = ActorSystem("KidsActorSystem")
 
-  val kid1 = system.actorOf(Props(new Kid(52)), name = "Kid1")
-  val kid2 = system.actorOf(Props(new Kid(54)), name = "Kid2")			
+	val kid1Props = Props(new Kid(52))
+	val kid2Props = Props(new Kid(54))
+
+	/*
+	You can watch other actor with ActorContext.watch(targetActorRef). 
+	To watching listening, invoke ActorContext.unwatch(targetActorRef)
+	*/
+
+  val backoffBuilder = 
+    (childName: String, childProbs: Props) => {
+	  	Backoff.onStop(
+	  		childProps = childProbs,
+	  		childName = childName,
+	  		minBackoff = 1.second,
+	  		maxBackoff = 16.seconds,
+	  		randomFactor = 0.0 // creates some noise to vary intervals (in %)
+	  	)
+	  }
+
+  val supervisor1 = BackoffSupervisor.props(
+  	backoffBuilder("Kid1", kid1Props)
+  )
+
+  val supervisor2 = BackoffSupervisor.props(
+  	backoffBuilder("Kid2", kid2Props)
+  )
+
+  val kid1 = system.actorOf(supervisor1, "Kid1Supervisor")
+  val kid2 = system.actorOf(supervisor2, "Kid2Supervisor")
 
 	kid1 ! Poke
 	kid2 ! Feed
