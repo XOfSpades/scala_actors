f8e1d7359d4e403c40107aa20af0f912062e26c6
Bernhard Stoecker <stoecki@der-ball-ist-rund.net>
2016-10-11 10:49:54 +0200

    Clean kidSpecs from unrelated code

--- src/test/scala/kidSpec.scala
+++ src/test/scala/kidSpec.scala
@@ -6,31 +6,11 @@ import akka.pattern.ask
 import akka.actor._
 
 import org.scalatest.FunSuite
-import scala.collection.mutable.Stack
  
 import kid.Kid
 import kid.Feed
 
 class KidSpec extends FunSuite {
- 
-  test("pop is invoked on a non-empty stack") { 
-    val stack = new Stack[Int]
-    stack.push(1)
-    stack.push(2)
-    val oldSize = stack.size
-    val result = stack.pop()
-    assert(result === 2)
-    assert(stack.size === oldSize - 1)
-  }
- 
-  test("pop is invoked on an empty stack") {
-    val emptyStack = new Stack[Int]
-    intercept[NoSuchElementException] {
-      emptyStack.pop()
-    }
-    assert(emptyStack.isEmpty)
-  }
-
   test ("A kid has kidSize after initializing") {
   	implicit val system = ActorSystem()
 
