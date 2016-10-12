693f237b3b174b66f6e786791a62d58d20e38473
Bernhard Stoecker <stoecki@der-ball-ist-rund.net>
2016-10-12 16:42:10 +0200

    Add tests for Poke reaction

--- src/test/scala/kidSpec.scala
+++ src/test/scala/kidSpec.scala
@@ -1,26 +1,47 @@
 package human
 
-import akka.testkit.TestActorRef
+import akka.testkit.{ TestActorRef, TestActors, TestKit, ImplicitSender }
 import scala.concurrent.duration._
 import scala.concurrent.Await
 import akka.pattern.ask
 
-import akka.actor._
+import akka.actor.{ ActorSystem, Actor, Props }
 
-import org.scalatest.FunSuite
+import org.scalatest.{ WordSpecLike, Matchers, BeforeAndAfterAll }
+
+class KidSpec extends TestKit(ActorSystem("KidSpec")) with ImplicitSender
+  with WordSpecLike with Matchers with BeforeAndAfterAll {
  
-import human.Kid
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+ 
+  "A kid" must {
+ 
+    "have a kidsize after initializing" in {
+      val testKidRef = TestActorRef(new Kid(52))
+      val testKid = testKidRef.underlyingActor
 
-class KidSpec extends FunSuite {
-  test ("A kid has kidSize after initializing") {
-  	implicit val system = ActorSystem()
+      assert(testKid.getKidSize == 52)
+    }
 
-  	val testKidRef = TestActorRef(new Kid(52))
-  	val testKid = testKidRef.underlyingActor
+    "grow when feeded" in {
+	    val testKidRef = TestActorRef(new Kid(52))
+	    val testKid = testKidRef.underlyingActor
 
-  	assert(testKid.getKidSize == 52)
+	    testKidRef ! Kid.Feed
+	    assert(testKid.getKidSize == 53)
+    }
+    
+    "menance the sender when poked" in {
+    	val testKidRef = TestActorRef(new Kid(52))
+	    val testKid = testKidRef.underlyingActor
 
-  	testKidRef ! Kid.Feed
-  	assert(testKid.getKidSize == 53)
-	}
+	    testKidRef ! Kid.Poke
+
+	    expectMsg(
+	      Adult.Menance("Stop that or I will sue you. This is child abuse!!!")
+	    )
+    } 
+  }
 }
